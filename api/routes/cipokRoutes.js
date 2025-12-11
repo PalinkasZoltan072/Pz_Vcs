809552ea1a3314252e604b848561ee92aadfc5fc
PalinkasZoltan072 <palinkas.zoltan@ckik.hu>
2025-12-11 01:36:30 +0100

    j

--- api/routes/cipokRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const cipokController = require("../controllers/cipokController");
-
-router.get("/", cipokController.getCipok);
-router.get("/:id", cipokController.getCipoById);
-
-module.exports = router;
--- api/routes/rendelesekRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const rendelesekController = require("../controllers/rendelesekController");
-
-router.get("/", rendelesekController.getRendelesek);
-router.get("/:id", rendelesekController.getRendelesById);
-
-module.exports = router;
