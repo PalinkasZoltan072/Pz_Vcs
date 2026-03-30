6d2f8e43c5e0693b4c56af1b9b835ebce674a23d
Varga Csaba <v.csaba@ckik.loc>
2026-03-30 08:29:54 +0200

    kesz minden

--- Pz_Vcskitörölt/Pz_Vcs/api/repositories/bankkartyaRepository.js
+++
@@ -1,8 +0,0 @@
-// const { DbError } = require("../errors");
-// class BankkartyaRepository {
-//     constructor(db) {
-//         // Itt kapjuk meg a Sequelize modelt ugyeeee ami még nincs
-//         this.bankkartya = db.bankkartya; 
-//     }
-// }
-// module.exports = BankkartyaRepository;
